ab54ddb494088fe0dc06601bc60a23d7201094d7
DrChhun <Chhunfindsome1@gmail.com>
2023-07-13 13:52:07 +0700

    finished book filiter on params

---
+++ app/Services/BookQuery.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+
+class BookQuery {
+    protected $safeParams = [
+        'id' => ['eq', 'gt', 'lt'],
+        'title' => ['eq'], //we decalear this to make sense cus title should be equal not greater or less than
+        'author' => ['eq'],
+        'totalPage' => ['eq', 'gt', 'lt'],
+    ];
+
+    protected $columnMap = [
+        'totalPage' => 'total_page'
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'gt' => '>'
+    ];
+
+    public function transform(Request $request) {
+        $eloQuery = []; //array to pass into eloquent
+
+        foreach($this->safeParams as $params => $operators) {
+            $query = $request->query($params);
+
+            if (!isset($query)) { //check query variable
+                continue;
+            }
+
+            $column = $this->columnMap[$params] ?? $params;
+
+            foreach($operators as $operator) {
+                if (isset($query[$operator])) { //chech if query isn't null
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+
+        return $eloQuery;
+    }
+}
