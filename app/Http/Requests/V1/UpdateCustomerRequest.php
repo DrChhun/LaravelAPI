a255cd8d9ce2680b6db31a0f5c255a2a421b67c9
DrChhun <Chhunfindsome1@gmail.com>
2023-07-14 13:59:38 +0700

    finished

--- app/Http/Requests/V1/UpdateCustomerRequest.php
+++ app/Http/Requests/V1/UpdateCustomerRequest.php
@@ -12,7 +12,9 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        $user = $this->user();
+
+        return $user != null && $user->tokenCan('update'); //check if token can update or not
     }
 
     /**
